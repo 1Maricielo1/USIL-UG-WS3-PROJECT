e13af2f117085629f3c7bbc822e3ffa25fe857a3
Angel Mejia <insaw@outlook.es>
2021-05-19 12:49:26 -0500

    ADDED: Estructura del proyecto

--- src/Controllers/Controller_Autor.java
+++ src/Controllers/Controller_Autor.java
@@ -1,13 +1,14 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates.
+ * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Beans;
+package Controllers;
 
 /**
  *
  * @author Insaw
  */
-public class Producto {
+public class Controller_Autor {
+    
 }
