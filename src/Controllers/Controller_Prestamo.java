35127687bbf2181133c29d33d360c2efd1e0e384
Maricielo <84482864+1Maricielo1@users.noreply.github.com>
2021-05-19 22:09:54 -0500

    Update Controller_Prestamo.java

--- src/Controllers/Controller_Prestamo.java
+++ src/Controllers/Controller_Prestamo.java
@@ -4,11 +4,19 @@
  * and open the template in the editor.
  */
 package Controllers;
+import Beans.Beans_Prestamo;
+import Models.Model_Prestamo;
+import java.util.List;
+
 
 /**
  *
  * @author Insaw
  */
 public class Controller_Prestamo {
+      public List<Beans_Prestamo> getListadoPrestamo(){
+        return new Model_Prestamo().getListadoPrestamo();
+      
+    }
     
 }
